42239d7dd35a497957bc5da3447f9d6e8ce135a1
Suchu <hiiamsuchi@gmail.com>
2022-05-21 09:50:34 +0530

    make it better

--- src/test/java/com/example/dao/InstructionRepositoryTest.java
+++ src/test/java/com/example/dao/InstructionRepositoryTest.java
@@ -1,30 +1,30 @@
-package com.example.dao;
-
-import com.example.model.Foodmenu;
-import com.example.model.Instruction;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
-class InstructionRepositoryTest {
-
-    @Autowired
-    private InstructionRepository instructionRepository;
-
-    @Test
-    void findById1(Integer id) {
-        Instruction f= new Instruction(1,"asf","dfgffh","fgullk","ghgj") ;
-        instructionRepository.save(f);
-
-        Optional<Instruction>  res=instructionRepository.findById(id);
-        assertThat(res).isNotNull();
-        assertThat(res).isEqualTo(f);
-
-    }
-
-}
+//package com.example.dao;
+//
+//import com.example.model.Foodmenu;
+//import com.example.model.Instruction;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class InstructionRepositoryTest {
+//
+//    @Autowired
+//    private InstructionRepository instructionRepository;
+//
+//    @Test
+//    void findById1(Integer id) {
+//        Instruction f= new Instruction(1,"asf","dfgffh","fgullk","ghgj") ;
+//        instructionRepository.save(f);
+//
+//        Optional<Instruction>  res=instructionRepository.findById(id);
+//        assertThat(res).isNotNull();
+//        assertThat(res).isEqualTo(f);
+//
+//    }
+//
+//}
